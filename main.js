871ee1ccae590f103ce90fe6251daa0ec26c17d9
vinimaffra03 <viniciushmdem15@gmail.com>
2025-01-10 23:44:47 -0300

    second commit

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
+
+// tirar ultimo elemento do array
+let popped = rows.pop();
+console.log(popped);
+
+rows.pop();
+console.log(rows);
+
+rows.push("welcome");
+console.log(rows);
